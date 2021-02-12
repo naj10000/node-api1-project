223a0960d3a85a3c33265a16d8f650379c1c8fc6
NaJ <naj10000@live.com>
2021-02-12 14:22:27 -0500

    mvp

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-
+server.listen(port, ()=> {
+    console.log('running at http://localhost:5000')
+})
 //some code
